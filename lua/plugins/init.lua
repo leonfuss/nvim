af8e3aeef914789f4d03302c2c99b0951c109752
Leon Fuß <leon.fuss@icloud.com>
2023-04-28 18:37:52 +0200

    initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		config = function()
+			require("mini.indentscope").setup({ symbol = "|" })
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = true,
+		event = "BufEnter",
+	},
+	{
+		enabled = false,
+		"lervag/vimtex",
+		lazy = false,
+	},
+}
