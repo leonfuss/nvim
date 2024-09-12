5353f4f6920a30cd79fca269ab52ab146b574475
Leon Fuss <leon.fuss@icloud.com>
2024-09-12 12:37:34 +0200

    current state

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -307,16 +307,6 @@ return {
 			})
 		end,
 	},
-	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
-	},
 	-- {
 	-- 	"zbirenbaum/copilot.lua",
 	-- 	event = "BufEnter",
